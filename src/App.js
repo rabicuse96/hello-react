4e2a65133ed1c5441ac6b6e8a4f11ff30e4d5652
Ahmad Muzakir <rabicuse96@gmail.com>
2019-07-03 07:07:17 +0700

    merubah app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render(){
+    return(
+      <div>
+        <h2>Makanan Khas Indonesia</h2>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
     <div>
+      <Header />
       <h1>Rendering Element</h1>
     </div>
   );
